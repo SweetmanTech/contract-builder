7e65bfd633248cd65b9c1185ad0a5700e05b19e7
SeniorDev <tech.eng.me@gmail.com>
2024-09-26 05:05:32 -0400

    I complete song name input tab

--- components/Header/BuilderMarks.tsx
+++ components/Header/BuilderMarks.tsx
@@ -10,6 +10,9 @@ const BuilderMarks = () => {
       {tab === CONTRACT_BUILDER_STEP.SPLITS_TYPE && (
         <Image src="/images/splits-type.svg" alt="" width={117} height={106} />
       )}
+      {tab === CONTRACT_BUILDER_STEP.SONG_NAME && (
+        <Image src="/images/song-name.svg" alt="" width={310} height={77} />
+      )}
       {tab === CONTRACT_BUILDER_STEP.SUCCESS && (
         <Image src="/images/success.svg" alt="" width={310} height={77} />
       )}
--- components/Pages/ContractBuilderPage/ContractBuilderPage.tsx
+++ components/Pages/ContractBuilderPage/ContractBuilderPage.tsx
@@ -1,4 +1,5 @@
 import BuildSuccess from '@/components/BuildSuccess'
+import SongNameForm from '@/components/SongNameForm'
 import SplitsTypes from '@/components/SplitsTypes'
 import { CONTRACT_BUILDER_STEP } from '@/hooks/useContractBuilder'
 import { useContractBuilderProvider } from '@/providers/ContractBuilderProvider'
@@ -9,6 +10,7 @@ const ContractBuilderPage = () => {
   return (
     <>
       {tab === CONTRACT_BUILDER_STEP.SPLITS_TYPE && <SplitsTypes />}
+      {tab === CONTRACT_BUILDER_STEP.SONG_NAME && <SongNameForm />}
       {tab === CONTRACT_BUILDER_STEP.SUCCESS && <BuildSuccess />}
     </>
   )
