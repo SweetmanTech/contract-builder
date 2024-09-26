e7675d652919f372dfa289ce7c9a3fa3ad318522
SeniorDev <tech.eng.me@gmail.com>
2024-09-26 03:58:52 -0400

    I add success page

--- components/BuildSuccess/BuildSuccess.tsx
+++ components/BuildSuccess/BuildSuccess.tsx
@@ -1,12 +1,15 @@
 import CreatedResult from './CreatedResult'
+import Descriptions from './Descriptions'
 
 const BuildSuccess = () => {
   return (
-    <div className="size-full grid grid-cols-12">
+    <div className="size-full grid grid-cols-12 gap-8">
       <div className="col-span-6 flex flex-col">
         <CreatedResult />
       </div>
-      <div className="col-span-6 flex flex-col justify-center gap-3"></div>
+      <div className="col-span-6 flex flex-col pt-40 gap-3 h-fit">
+        <Descriptions />
+      </div>
     </div>
   )
 }
