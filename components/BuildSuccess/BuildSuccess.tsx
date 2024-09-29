a6c69db737f5af7d131c243ab5d47393402ff1c3
codesage <laughingsagecoder@gmail.com>
2024-09-29 21:51:34 +0200

    resolving responsive issues in success tab

--- components/BuildSuccess/BuildSuccess.tsx
+++ components/BuildSuccess/BuildSuccess.tsx
@@ -3,11 +3,11 @@ import Descriptions from './Descriptions'
 
 const BuildSuccess = () => {
   return (
-    <div className="size-full grid grid-cols-12 gap-8">
-      <div className="col-span-6 flex flex-col">
+    <div className="size-full grid grid-cols-12 sm:gap-8">
+      <div className="col-span-12 flex flex-col sm:col-span-6">
         <CreatedResult />
       </div>
-      <div className="col-span-6 flex flex-col pt-40 gap-3 h-fit">
+      <div className="col-span-6 h-fit hidden flex-col pt-40 gap-3 sm:flex">
         <Descriptions />
       </div>
     </div>
