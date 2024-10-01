4fd8747ae982089240eae07eda7167379cc23965
Nick Carney <npcarney@uvm.edu>
2024-10-01 14:43:46 -0400

    fix page.tsx to use LandingPage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 'use client'
 
 import React from 'react'
-import ContractBuilderPage from '@/components/Pages/ContractBuilderPage'
+import LandingPage from '@/components/Pages/LandingPage'
 
 export default function Page() {
-  return <ContractBuilderPage />
+  return <LandingPage />
 }
