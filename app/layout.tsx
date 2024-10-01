7722e77606537822dd07eb8021af1f246eaa68de
Nick Carney <npcarney@uvm.edu>
2024-10-01 14:38:35 -0400

    adding in page and layout

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import Header from '@/components/Header'
+import Providers from '@/providers/Providers'
+import React from 'react'
+import '@/styles/global.css'
+import Image from 'next/image'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className="overflow-hidden relative">
+        <Image
+          src="/images/background.png"
+          alt=""
+          width={928}
+          height={928}
+          className="absolute right-[-200px] bottom-[-320px] z-[1] pointer-events-none"
+        />
+        <main className="pt-[180px] px-14 h-screen w-screen overflow-y-auto overflow-x-hidden pb-20 relative z-[1000]">
+          <Providers>
+            <Header />
+            {children}
+          </Providers>
+        </main>
+      </body>
+    </html>
+  )
+}
