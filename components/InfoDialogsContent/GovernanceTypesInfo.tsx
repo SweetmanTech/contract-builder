0eeac9e22a35e3cad3deb40f99a789e7027b3fa6
Talha <talhamughal600@gmail.com>
2024-10-09 02:39:54 +0500

    extract one component per file

--- components/InfoDialogsContent/GovernanceTypesInfo.tsx
+++ components/InfoDialogsContent/GovernanceTypesInfo.tsx
@@ -1,49 +1,8 @@
 import { useState } from 'react'
-import InfoDialog from './InfoDialog'
-import ReadHereLink from './ReadHereLink'
+import ReadHereLink from '../ReadHereLink'
+import InfoDialog from '../InfoDialog'
 
-export const SplitsTypesInfo = () => (
-  <>
-    In music industry, there are two primary types of works:{' '}
-    <b>Musical Compositions</b> and{' '}
-    <b>
-      Sound Recordings <s>copyright and</s>
-    </b>{' '}
-    <b>
-      <s>publishing rights.</s>
-    </b>{' '}
-    These two kinds of works have their own copyright.
-    <ol className="list-decimal list-outside ml-6 mb-8">
-      <li>
-        <b>
-          Copyright in Musical Composition (Publishing Rights or Composition
-          Rights):
-        </b>{' '}
-        Relate to the “song” or underlying composition—the lyrics and melody of
-        a song, independent of any particular recording. These rights are
-        divided between the <b>songwriter</b> (or composer) and the{' '}
-        <b>music publisher.</b> The publisher manages the songwriter’s
-        composition by licensing it for use, collecting royalties, and ensuring
-        it is properly credited. The key components of publishing rights include{' '}
-        <b>performance rights, mechanical rights,</b> and synchronization rights
-        (for use in films, TV, etc.).
-      </li>
-      <li>
-        <b>Copyright in Sound Recordings (MASTER Rights):</b> Pertains to the
-        ownership of a particular recording of a performance of a song. Whoever
-        owns the master rights controls the use, distribution, reproduction, and
-        performance of that specific recording. These rights typically belong to
-        the record label or the artist who financed the production of the
-        recording, though they can be sold or licensed.
-      </li>
-    </ol>
-    Both rights are crucial for monetizing and legally protecting music. The
-    copyright controls the use of a specific recording, while publishing rights
-    control the use of the song&#39;s composition.
-  </>
-)
-
-export const GovernanceTypesInfo = () => {
+const GovernanceTypesInfo = () => {
   const [isVotingInfoOpen, setIsVotingInfoOpen] = useState(false)
   const [isAdminInfoOpen, setIsAdminInfoOpen] = useState(false)
 
@@ -139,3 +98,5 @@ export const GovernanceTypesInfo = () => {
     </>
   )
 }
+
+export default GovernanceTypesInfo
