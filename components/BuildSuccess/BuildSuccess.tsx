b5b9ae249eb29ffcd6a122a8c0b4de390f7bf413
Muhammad Farman <60259603+mohhomadfarman@users.noreply.github.com>
2024-10-11 01:40:48 +0530

    pull update

--- components/BuildSuccess/BuildSuccess.tsx
+++ components/BuildSuccess/BuildSuccess.tsx
@@ -5,7 +5,7 @@ const BuildSuccess = () => {
   return (
     <div className="md:size-full grid grid-cols-12 gap-6 mb-5">
       <div className="md:col-span-6 col-span-12 flex flex-col md:justify-end justify-start relative">
-        <CreatedResult />
+         <CreatedResult />
       </div>
       <div className="col-span-6 md:flex hidden flex-col justify-center gap-3 ">
         <Descriptions />
