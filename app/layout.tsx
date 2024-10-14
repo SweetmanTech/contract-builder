ca03da40964f16f6b2946c4649a81b6427d42056
Talha <talhamughal600@gmail.com>
2024-10-14 13:07:55 +0500

    google-analtics-tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,29 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+    
+    <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-T7ZGQBC8');
+            `,
+          }}
+        />
+      </head>
       <body className="relative overflow-hidden">
+      <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T7ZGQBC8"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <Image
           src="/images/background.png"
           alt="Background"
