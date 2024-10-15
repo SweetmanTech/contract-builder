bca82addf3c3122421470ac48b850bc006eb2590
Talha <talhamughal600@gmail.com>
2024-10-16 03:20:39 +0500

    refactor code

--- components/Header/BuilderMarks.tsx
+++ components/Header/BuilderMarks.tsx
@@ -2,6 +2,27 @@ import { CONTRACT_BUILDER_STEP } from '@/hooks/useContractBuilder'
 import { useContractBuilderProvider } from '@/providers/ContractBuilderProvider'
 import Image from 'next/image'
 
+const Dot = ({
+  image,
+  className,
+  onClick,
+}: {
+  image: string
+  className: string
+  onClick: () => void
+}) => {
+  return (
+    <div className="relative">
+      <Image src={image} alt="" width={310} height={77} />
+
+      <div
+        className={`absolute top-1/2 -translate-y-1/2 size-6 bg-transparent cursor-pointer ${className}`}
+        onClick={onClick}
+      />
+    </div>
+  )
+}
+
 const BuilderMarks = () => {
   const { tab } = useContractBuilderProvider()
   const { setTab } = useContractBuilderProvider()
@@ -12,110 +33,53 @@ const BuilderMarks = () => {
         <Image src="/images/splits-type.svg" alt="" width={117} height={106} />
       )}
       {tab === CONTRACT_BUILDER_STEP.SONG_NAME && (
-        <>
-          <div className="relative">
-            <Image src="/images/song-name.svg" alt="" width={310} height={77} />
-
-            <div
-              className="absolute top-1/2 left-0 -translate-y-1/2 size-6 bg-transparent cursor-pointer"
-              onClick={() => setTab(CONTRACT_BUILDER_STEP.SPLITS_TYPE)}
-            />
-          </div>
-        </>
+        <Dot
+          image="/images/song-name.svg"
+          className="left-0"
+          onClick={() => setTab(CONTRACT_BUILDER_STEP.SPLITS_TYPE)}
+        />
       )}
       {tab === CONTRACT_BUILDER_STEP.COLLABORATORS_AMOUNT && (
-        <>
-          <div className="relative">
-            <Image
-              src="/images/collaborators-amount.svg"
-              alt=""
-              width={310}
-              height={77}
-            />
-
-            <div
-              className="absolute top-1/2 left-12 -translate-y-1/2 size-6 bg-transparent cursor-pointer"
-              onClick={() => setTab(CONTRACT_BUILDER_STEP.SONG_NAME)}
-            />
-          </div>
-        </>
+        <Dot
+          image="/images/collaborators-amount.svg"
+          className="left-12"
+          onClick={() => setTab(CONTRACT_BUILDER_STEP.SONG_NAME)}
+        />
       )}
       {tab === CONTRACT_BUILDER_STEP.COLLABORATOR_INPUT && (
-        <>
-          <div className="relative">
-            <Image
-              src="/images/collaborator-input.svg"
-              alt=""
-              width={310}
-              height={77}
-            />
-
-            <div
-              className="absolute top-1/2 left-24 -translate-y-1/2 size-6 bg-transparent cursor-pointer"
-              onClick={() => setTab(CONTRACT_BUILDER_STEP.COLLABORATORS_AMOUNT)}
-            />
-          </div>
-        </>
+        <Dot
+          image="/images/collaborator-input.svg"
+          className="left-24"
+          onClick={() => setTab(CONTRACT_BUILDER_STEP.COLLABORATORS_AMOUNT)}
+        />
       )}
       {tab === CONTRACT_BUILDER_STEP.GOVERNANCE_TYPE && (
-        <>
-          <div className="relative">
-            <Image
-              src="/images/governance-type.svg"
-              alt=""
-              width={310}
-              height={77}
-            />
-
-            <div
-              className="absolute top-1/2 left-34 -translate-y-1/2 size-6 bg-transparent cursor-pointer"
-              onClick={() => setTab(CONTRACT_BUILDER_STEP.COLLABORATOR_INPUT)}
-              style={{ left: '144px' }}
-            />
-          </div>
-        </>
+        <Dot
+          image="/images/governance-type.svg"
+          className="left-36"
+          onClick={() => setTab(CONTRACT_BUILDER_STEP.COLLABORATOR_INPUT)}
+        />
       )}
       {tab === CONTRACT_BUILDER_STEP.VOTE && (
-        <>
-          <div className="relative">
-            <Image src="/images/vote.svg" alt="" width={310} height={77} />
-
-            <div
-              className="absolute top-1/2 left-48 -translate-y-1/2 size-6 bg-transparent cursor-pointer"
-              onClick={() => setTab(CONTRACT_BUILDER_STEP.GOVERNANCE_TYPE)}
-            />
-          </div>
-        </>
+        <Dot
+          image="/images/vote.svg"
+          className="left-48"
+          onClick={() => setTab(CONTRACT_BUILDER_STEP.GOVERNANCE_TYPE)}
+        />
       )}
       {tab === CONTRACT_BUILDER_STEP.DESIGNATE_ADMIN && (
-        <>
-          <div className="relative">
-            <Image
-              src="/images/designate-admin.svg"
-              alt=""
-              width={310}
-              height={77}
-            />
-            <div
-              className="absolute top-1/2 left-60 -translate-y-1/2 size-6 bg-transparent cursor-pointer"
-              onClick={() => setTab(CONTRACT_BUILDER_STEP.VOTE)}
-              style={{ left: '190px' }}
-            />
-          </div>
-        </>
+        <Dot
+          image="/images/designate-admin.svg"
+          className="left-[184px]"
+          onClick={() => setTab(CONTRACT_BUILDER_STEP.GOVERNANCE_TYPE)}
+        />
       )}
       {tab === CONTRACT_BUILDER_STEP.SUCCESS && (
-        <>
-          <div className="relative">
-            <Image src="/images/success.svg" alt="" width={310} height={77} />
-
-            <div
-              className="absolute top-1/2 -translate-y-1/2 size-6 bg-transparent cursor-pointer"
-              onClick={() => setTab(CONTRACT_BUILDER_STEP.DESIGNATE_ADMIN)}
-              style={{ left: '240px', top: '10px' }}
-            />
-          </div>
-        </>
+        <Dot
+          image="/images/success.svg"
+          className="left-[234px] top-[10px]"
+          onClick={() => setTab(CONTRACT_BUILDER_STEP.DESIGNATE_ADMIN)}
+        />
       )}
     </>
   )
