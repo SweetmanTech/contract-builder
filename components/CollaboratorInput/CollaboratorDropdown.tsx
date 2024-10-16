74040dbb899d5ac6f5f30b98d61448b4c3518a41
Talha <talhamughal600@gmail.com>
2024-10-16 19:19:58 +0500

    refactor code

---
+++ components/CollaboratorInput/CollaboratorDropdown.tsx
@@ -0,0 +1,42 @@
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+} from '@/components/ui/dropdown'
+import { ChevronDownIcon } from 'lucide-react'
+
+export default function CollaboratorDropdown({
+  contributionType,
+  options,
+  setContributionType,
+}: {
+  contributionType: string
+  options: string[]
+  setContributionType: (value: string) => void
+}) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className="flex justify-between w-1/2 items-center text-left bg-black text-[#9ca3af] font-rubik rounded-md border-white border p-2 cursor-pointer">
+          {contributionType ? contributionType : 'Select options'}
+          <ChevronDownIcon className="size-5 text-white" />
+        </button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent className="bg-black w-80 font-rubik">
+        <DropdownMenuRadioGroup
+          value={contributionType}
+          onValueChange={setContributionType}
+        >
+          {options.map((option) => (
+            <DropdownMenuRadioItem key={option} value={option}>
+              {option}
+            </DropdownMenuRadioItem>
+          ))}
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
--- components/CollaboratorInput/CollaboratorTypeDropdown.tsx
+++ components/CollaboratorInput/CollaboratorTypeDropdown.tsx
@@ -1,50 +1,9 @@
 import React from 'react'
 import { useContractBuilderProvider } from '@/providers/ContractBuilderProvider'
-import {
-  DropdownMenu,
-  DropdownMenuTrigger,
-  DropdownMenuContent,
-  DropdownMenuRadioGroup,
-  DropdownMenuRadioItem,
-} from '@/components/ui/dropdown'
-import { ChevronDownIcon } from 'lucide-react'
+import CollaboratorDropdown from './CollaboratorDropdown'
 
 const masterOptions = ['Artist', 'Producer', 'Executive producer', 'Engineer']
 const songWritingOptions = ['Lyrics', 'Music', 'Both']
-
-function CollaboratorDropdown({
-  contributionType,
-  options,
-  setContributionType,
-}: {
-  contributionType: string
-  options: string[]
-  setContributionType: (value: string) => void
-}) {
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <button className="flex justify-between w-1/2 items-center text-left bg-black text-[#9ca3af] font-rubik rounded-md border-white border p-2 cursor-pointer">
-          {contributionType ? contributionType : 'Select options'}
-          <ChevronDownIcon className="size-5 text-white" />
-        </button>
-      </DropdownMenuTrigger>
-
-      <DropdownMenuContent className="bg-black w-80 font-rubik">
-        <DropdownMenuRadioGroup
-          value={contributionType}
-          onValueChange={setContributionType}
-        >
-          {options.map((option) => (
-            <DropdownMenuRadioItem key={option} value={option}>
-              {option}
-            </DropdownMenuRadioItem>
-          ))}
-        </DropdownMenuRadioGroup>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  )
-}
 
 export default function CollaboratorTypeDropdown() {
   const {
