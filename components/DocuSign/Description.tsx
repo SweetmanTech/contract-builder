30325209ec89b438358fbf5a5483cd330a5da863
Faisal25DEC <faisalmd25121999@gmail.com>
2024-10-29 08:25:14 +0530

    docu sign page

--- components/DocuSign/Description.tsx
+++ components/DocuSign/Description.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Description = () => {
   return (
     <div className="font-share w-[95%] self-center">
-      <p className="md:text-3xl !leading-[4rem]">
+      <p className="md:text-3xl md:leading-[3.25rem]">
         With this option, contracts will be automatically sent out through
         DocuSign to all collaborators on the project with 72 hours. Once all the
         collaborators have completed signing, you will have the ability to
