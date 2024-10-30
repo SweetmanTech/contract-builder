9ac29400f445b31ebed656094c0baf83fcbbaa6c
Faisal25DEC <faisalmd25121999@gmail.com>
2024-10-30 18:52:22 +0530

    footer logo on mobile

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,15 +3,27 @@
 import { usePathname } from 'next/navigation'
 import React from 'react'
 import BuilderMarks from '../Header/BuilderMarks'
+import Image from 'next/image'
 
 const Footer = () => {
   const path = usePathname()
 
   const showBuilderMarks =
     path.includes('/contract-builder') || path.includes('/docusign')
+  const showFooterLogo = path.includes('/docusign')
   return (
-    <div className="w-11/12  justify-center items-center flex md:hidden ">
+    <div className="w-11/12  justify-center items-center flex flex-col gap-12 md:hidden ">
       {showBuilderMarks && <BuilderMarks />}
+      {showFooterLogo && (
+        <div className=" md:hidden">
+          <Image
+            src="/images/footer-logo.svg"
+            width={52}
+            height={26}
+            alt="Footer Logo"
+          />
+        </div>
+      )}
     </div>
   )
 }
