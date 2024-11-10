387d1768b77ca0d7e8030f0f0b49c92334bdb90f
Faisal25DEC <faisalmd25121999@gmail.com>
2024-11-10 17:07:40 +0530

    complete MYC-407

--- components/BuildSuccess/Descriptions.tsx
+++ components/BuildSuccess/Descriptions.tsx
@@ -1,11 +1,9 @@
+const descriptionParagraph = `Don't forget to review it with your colleagues or a lawyer, customize it if needed, and sign it when you're ready!`
 const Descriptions = () => {
   return (
-    <>
-      <p className="text-white font-share md:text-[32px] tracking-[-0.05rem] pt-40">
-        Don´t forget to review it with your colleagues or with a lawyer,
-        customize it if needed and sign it when you are ready!
-      </p>
-    </>
+    <p className="text-white font-share md:text-[32px] tracking-[-0.05rem] pt-40">
+      {descriptionParagraph}
+    </p>
   )
 }
 
