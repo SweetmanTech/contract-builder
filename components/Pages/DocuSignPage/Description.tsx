408030458441b137fd53558a547bc4e2ba62b326
Faisal25DEC <faisalmd25121999@gmail.com>
2024-11-11 23:15:22 +0530

    change variable name

--- components/Pages/DocuSignPage/Description.tsx
+++ components/Pages/DocuSignPage/Description.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Description = () => {
-  const descParagraph = `With this option, the contract will be automatically sent out through DocuSign to all collaborators on the project within 48 hours. Once everyone has completed signing, all parties will be able to download the fully signed contracts to ensure proper protection.`
+  const description = `With this option, the contract will be automatically sent out through DocuSign to all collaborators on the project within 48 hours. Once everyone has completed signing, all parties will be able to download the fully signed contracts to ensure proper protection.`
   return (
     <div className="font-share w-full md:w-4/5 self-start flex flex-col gap-10">
       <p className="md:text-[32px] md:leading-[48px] text-left">
-        {descParagraph}
+        {description}
       </p>
     </div>
   )
