eaff16ba063d0cf1d868a7eaf7c0215b2c9c6f59
Faisal25DEC <faisalmd25121999@gmail.com>
2024-11-12 21:10:58 +0530

    change variable name

--- components/BuildSuccess/Descriptions.tsx
+++ components/BuildSuccess/Descriptions.tsx
@@ -1,8 +1,8 @@
-const descriptionParagraph = `Don't forget to review it with your colleagues or a lawyer, customize it if needed, and sign it when you're ready!`
+const description = `Don't forget to review it with your colleagues or a lawyer, customize it if needed, and sign it when you're ready!`
 const Descriptions = () => {
   return (
     <p className="text-white font-share md:text-[32px] tracking-[-0.05rem] pt-40">
-      {descriptionParagraph}
+      {description}
     </p>
   )
 }
