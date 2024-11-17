d48ba9879d7d384c566d8572368b94010cf233ef
Faisal25DEC <faisalmd25121999@gmail.com>
2024-11-17 07:12:14 +0530

    using navbar on mobile

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,14 +4,27 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import BuilderMarks from './BuilderMarks'
+import { useContractBuilderProvider } from '@/providers/ContractBuilderProvider'
+import { getMobileClassesBasedOnSteps } from '@/lib/constants/header'
 
 const Header = () => {
-  const isBuilder = usePathname().includes('/contract-builder')
+  const path = usePathname()
+  const { tab } = useContractBuilderProvider()
 
+  const isBuilder = path.includes('/contract-builder')
+
+  const mobileClassesBasedOnSteps = getMobileClassesBasedOnSteps({
+    tab,
+    path,
+  })
   return (
-    <header className="fixed w-full px-6 min-h-[87px] pt-4 md:pt-0 flex flex-col md:flex-row md:justify-between items-center top-0 left-0 border-b border-white-light bg-black z-50">
-      <Link href="/" className="md:order-1 order-3">
-        <div className="w-32 h-auto md:w-48 md:h-auto lg:w-50 lg:h-auto">
+    <header
+      className={`fixed w-full min-h-fit py-3 md:pt-0 flex ${mobileClassesBasedOnSteps.header} md:flex-row md:justify-between items-center top-0 left-0 border-b border-white-light bg-black z-50`}
+    >
+      <Link href="/">
+        <div
+          className={`w-32 h-auto md:w-48 md:h-auto lg:w-50 lg:h-auto -ml-2`}
+        >
           <Image
             src="/images/logo-dark.svg"
             alt="Logo"
@@ -22,20 +35,21 @@ const Header = () => {
         </div>
       </Link>
 
-      <div className="order-1 md:order-3 text-center mb-1 md:mb-0">
-        <p className="uppercase font-rubik text-2xl md:text-3xl">
+      {isBuilder && (
+        <div className="hidden md:block">
+          <BuilderMarks />
+        </div>
+      )}
+      <div className="text-center mb-1 md:mb-0">
+        <p
+          className={`uppercase font-rubik ${mobileClassesBasedOnSteps?.musicSplits} md:text-3xl`}
+        >
           Music Splits
         </p>
         <p className="font-share text-base md:text-lg tracking-wider">
           Contract Builder
         </p>
       </div>
-
-      {isBuilder && (
-        <div className="order-2 hidden md:block">
-          <BuilderMarks />
-        </div>
-      )}
     </header>
   )
 }
