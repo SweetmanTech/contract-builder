6b7c1c3896bdca7d6fce459e42b578d0b7225ce7
Faisal25DEC <faisalmd25121999@gmail.com>
2024-11-17 06:09:21 +0530

    fix:mobille scrolling on safari

--- components/CollaboratorInput/CollaboratorInput.tsx
+++ components/CollaboratorInput/CollaboratorInput.tsx
@@ -3,11 +3,11 @@ import SubmitForm from './SubmitForm'
 
 const CollaboratorInput = () => {
   return (
-    <div className="md:size-full grid grid-cols-12 gap-6">
-      <div className="md:col-span-6 col-span-12 flex flex-col md:justify-end justify-start relative mb-16">
+    <div className="md:size-full flex gap-6">
+      <div className="md:w-1/2 flex flex-col md:justify-end justify-start relative mb-16">
         <SubmitForm />
       </div>
-      <div className="col-span-6 md:flex hidden flex-col justify-center gap-3">
+      <div className="w-1/2 md:flex hidden flex-col justify-center gap-3">
         <Descriptions />
       </div>
     </div>
