9227e19e2400f07e8bd7584a6d399bfc9e305da1
Nick Carney <npcarney@uvm.edu>
2024-12-06 14:18:43 -0500

    collabPage amount

--- hooks/useCollaboratorsAmount.tsx
+++ hooks/useCollaboratorsAmount.tsx
@@ -22,14 +22,15 @@ const useCollaboratorsAmount = () => {
       intValue = 0
     }
 
+    setSongWritingCollaboratorsAmount(0)
+    setMasterRecordingCollaboratorsAmount(0)
+
     switch (type) {
       case SPLIT_TYPE.SONG_WRITING:
         setSongWritingCollaboratorsAmount(intValue)
-        setMasterRecordingCollaboratorsAmount(0)
         break
 
       case SPLIT_TYPE.MASTER_RECORDING:
-        setSongWritingCollaboratorsAmount(0)
         setMasterRecordingCollaboratorsAmount(intValue)
         break
     }
